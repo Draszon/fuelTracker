2bbe2840c5d52c523571eff9c03ed4a063544c86
Szentgyörgyi Péter <peti0202@windowslive.com>
2025-12-04 07:35:07 +0100

    statisztika oldal design

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -3,16 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\Car;
+use App\Models\Fuel;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class StatisticsController extends Controller
 {
     public function index() {
-                
+        //a főoldalon megjelenő adatok lekérdezése és szűrése
+        //alapértelmezetten: kocsi, üzemanyag, szerviz, biztosítás
+        $cars = Car::all();
+
+        $now = Carbon::now();
+
+        $fuelMoneyMonthSum = Fuel::whereYear('date', $now->year)
+            ->whereMonth('date', $now->month)
+            ->sum('money');
 
         return Inertia::render('Statistics', [
-
+            'cars' => $cars,
+            'fuelMoneyMonthSum' => $fuelMoneyMonthSum,
         ]);
     }
 }
--- resources/js/Pages/Statistics.vue
+++ resources/js/Pages/Statistics.vue
@@ -4,7 +4,7 @@ import { Head, useForm, router, usePage } from '@inertiajs/vue3';
 import { ref, onMounted, computed } from 'vue';
 
 defineProps({
-
+  cars: Array
 });
 
 </script>
@@ -15,7 +15,87 @@ defineProps({
 </Head>
 <PublicLayout>
 
+<section class="my-10">
+  <div class="bg-white py-10 rounded-md shadow-sm w-full max-w-[1280px] xl:mx-auto">
+    <div class="px-2 xl:px-10">
+      <div class="overflow-x-auto">
+        <h2 class="font-bold text-2xl mb-5">Üzemanyag statisztika</h2>
 
+        <h2 class="font-bold text-2xl">Havi</h2>
+        <div class="px-2 mb-10 rounded-lg sm:flex sm:flex-wrap sm:gap-5">
+          <div class="bg-gray-100 rounded-2xl shadow-sm w-full p-5 my-5 sm:max-w-[350px]">
+            <h3 class="text-xl font-medium">Össz. üzemanyag</h3>
+            <p class="text-[30px]">89,54 l</p>
+          </div>
+
+          <div class="bg-gray-100 rounded-2xl shadow-sm w-full p-5 my-5 sm:max-w-[350px]">
+            <h3 class="text-xl font-medium">Össz. megtett km</h3>
+            <p class="text-[30px]">1154 km</p>
+          </div>
+
+          <div class="bg-gray-100 rounded-2xl shadow-sm w-full p-5 my-5 sm:max-w-[350px]">
+            <h3 class="text-xl font-medium">Össz.  költség</h3>
+            <p class="text-[30px]">86591 Ft</p>
+          </div>
+
+          <div class="bg-gray-100 rounded-2xl shadow-sm w-full p-5 my-5 sm:max-w-[350px]">
+            <h3 class="text-xl font-medium">Átlagfogyasztás</h3>
+            <p class="text-[30px]">6,3 l/100km</p>
+          </div>
+        </div>
+
+        <h2 class="font-bold text-2xl">Éves</h2>
+        <div class="px-2 rounded-lg sm:flex sm:flex-wrap sm:gap-5">
+          <div class="bg-gray-100 rounded-2xl shadow-sm w-full p-5 my-5 sm:max-w-[350px]">
+            <h3 class="text-xl font-medium">Össz. üzemanyag</h3>
+            <p class="text-[30px]">89,54 l</p>
+          </div>
+
+          <div class="bg-gray-100 rounded-2xl shadow-sm w-full p-5 my-5 sm:max-w-[350px]">
+            <h3 class="text-xl font-medium">Össz. megtett km</h3>
+            <p class="text-[30px]">1154 km</p>
+          </div>
+
+          <div class="bg-gray-100 rounded-2xl shadow-sm w-full p-5 my-5 sm:max-w-[350px]">
+            <h3 class="text-xl font-medium">Össz.  költség</h3>
+            <p class="text-[30px]">86591 Ft</p>
+          </div>
+
+          <div class="bg-gray-100 rounded-2xl shadow-sm w-full p-5 my-5 sm:max-w-[350px]">
+            <h3 class="text-xl font-medium">Átlagfogyasztás</h3>
+            <p class="text-[30px]">6,3 l/100km</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="my-10">
+  <div class="bg-white py-10 rounded-md shadow-sm w-full max-w-[1280px] xl:mx-auto">
+    <div class="px-2 xl:px-10">
+      <div class="overflow-x-auto">
+        <h2 class="font-bold text-2xl mb-5">Szerviz statisztika</h2>
+
+        <h2 class="font-bold text-2xl">Havi</h2>
+        <div class="px-2 mb-10 rounded-lg sm:flex sm:flex-wrap sm:gap-5">
+          <div class="bg-gray-100 rounded-2xl shadow-sm w-full p-5 my-5 sm:max-w-[350px]">
+            <h3 class="text-xl font-medium">Össz. szervizköltség</h3>
+            <p class="text-[30px]">32500 Ft</p>
+          </div>
+        </div>
+
+        <h2 class="font-bold text-2xl">Éves</h2>
+        <div class="px-2 mb-10 rounded-lg sm:flex sm:flex-wrap sm:gap-5">
+          <div class="bg-gray-100 rounded-2xl shadow-sm w-full p-5 my-5 sm:max-w-[350px]">
+            <h3 class="text-xl font-medium">Össz. szervizköltség</h3>
+            <p class="text-[30px]">158437 Ft</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 </PublicLayout>
 </template>
