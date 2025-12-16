b63b6351e56681c64bf613b89ad570b6b3ace21b
Szentgyörgyi Péter <peti0202@windowslive.com>
2025-12-16 17:53:52 +0100

    migrációs bugok javítása

--- database/migrations/2025_11_21_051430_create_cars_table.php
+++ database/migrations/2025_11_21_051430_create_cars_table.php
@@ -16,10 +16,17 @@ return new class extends Migration
             $table->string('name');
             $table->string('licence_plate');
             $table->string('car_type');
+            $table->decimal('average_fuel_consumption', 5, 1);
             $table->integer('year');
+            $table->integer('current_km');
             $table->integer('oil_change_cycle_km');
             $table->integer('oil_change_cycle_year');
-            $table->integer('break_oil_cycle_km');
+            $table->integer('break_oil_cycle_year');
+            $table->date('inspection_valid_from');
+            $table->date('inspection_valid_until');
+            $table->integer('last_oil_change_km');
+            $table->date('last_oil_change_date');
+            $table->date('last_break_oil_change_date');
             $table->timestamps();
         });
     }
